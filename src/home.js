fa7f992c4d49b710180d825bbecded237da42ec5
halifaxnagujarati <147547856+halifaxnagujarati@users.noreply.github.com>
2023-10-17 14:49:31 -0300

    totalHoursByHourType works automatically

--- src/home.js
+++ src/home.js
@@ -1,91 +1,78 @@
-import React, { useState, useEffect } from 'react';
-import './home.css';
+import { useState, useEffect } from "react";
+import * as React from 'react';
+import Button from '@mui/material/Button';
 
 const Home = ({ firstName }) => {
   const [selectedWeek, setSelectedWeek] = useState(null);
   const [tableData, setTableData] = useState([]);
+  const [totals, setTotals] = useState({
+    RH: 0, TL: 0, TLNS: 0, NA: 0, NS: 0, SH: 0, SNS: 0, VHRH: 0, VHNH: 0
+  });
+
+  useEffect(() => {
+    calculateTotalHoursByType();
+  }, [tableData]);
 
   const formatDateRange = (startDate, endDate) => {
     const start = new Date(startDate);
     const end = new Date(endDate);
-
-    const startFormatted = start.toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' });
-    const endFormatted = end.toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' });
-
-    return `${startFormatted} - ${endFormatted}`;
+    return `${start.toLocaleDateString()} - ${end.toLocaleDateString()}`;
   }
 
-  
-const calculateTotalHoursByType = (tableData) => {
+  const calculateTotalHoursByType = () => {
     let totals = {
-        RH: 0, // Regular Hours, Training Hours, On Call Regular
-        TL: 0, // Team Lead Regular, Team Lead Night Awake
-        TLNS: 0, // Team Lead Sleep
-        NA: 0, // Night Awake Hours
-        NS: 0, // Night Sleep Hours
-        SH: 0, // Sick Hours
-        SNS: 0, // Sick Night Sleep Hours
-        VHRH: 0, // Vacation Regular Hours
-        VHNS: 0, // Vacation Night Sleep Hours
+      RH: 0, TL: 0, TLNS: 0, NA: 0, NS: 0, SH: 0, SNS: 0, VHRH: 0, VHNH: 0
     };
 
     tableData.forEach((row) => {
-        row.entries.forEach((entry) => {
-            const hour = calculateHours(entry.startTime, entry.endTime);
-            if (!isNaN(hour)){
-                switch (entry.hourType) {
-                    case 'Regular Hours':
-                    case 'Training Hours':
-                    case 'On Call Regular':
-                        totals.RH += hour;
-                        break;
-                    case 'Team Lead Regular':
-                    case 'Team Lead Night Awake':
-                        totals.TL += hour;
-                        break;
-                    case 'Team Lead Sleep':
-                        totals.TLNS += hour;
-                        break;
-                    case 'Night Awake Hours':
-                        totals.NA += hour;
-                        break;
-                    case 'Night Sleep Hours':
-                        totals.NS += hour;
-                        break;
-                    case 'Sick Hours':
-                        totals.SH += hour;
-                        break;
-                    case 'Sick Night Sleep Hours':
-                        totals.SNS += hour;
-                        break;
-                    case 'Vacation Regular Hours':
-                        totals.VHRH += hour;
-                        break;
-                    case 'Vacation Night Sleep Hours':
-                        totals.VHNS += hour;
-                        break;
-                }
-            }
-        });
-    });
-
-    return totals;
-  };
-
-
-  const calculateTotalHours = (hourType, tableData) => {
-    let totalHours = 0;
-    tableData.forEach((row) => {
       row.entries.forEach((entry) => {
-        if (entry.hourType === hourType) {
-          const hours = calculateHours(entry.startTime, entry.endTime);
-          if (!isNaN(hours)){
-            totalHours += hours;
-          }
+        const hour = calculateHours(entry.startTime, entry.endTime);
+        switch (entry.hourType) {
+          case 'Regular Hours':
+          case 'Training Hours':
+          case 'On Call Regular':
+            totals.RH += hour;
+            break;
+          case 'Team Lead Regular':
+          case 'Team Lead Night Awake':
+            totals.TL += hour;
+            break;
+        case 'Team Lead Sleep':
+          totals.TLNS += hour;
+          break;
+        case 'Night Awake Hours':
+          totals.NA += hour;
+          break;
+        case 'Night Sleep Hours':
+          case 'On Call Night Sleep':
+          totals.NS += hour;
+          break;
+        case 'Sick Hours (Regular)':
+          case 'Sick Hours (Night Awake)':
+          totals.SH += hour;
+          break;
+        case 'Sick Hours (Night Sleep)':
+          totals.SNS += hour;
+          break;
+        case 'Vacation Hours (Regular)':
+          totals.VHRH += hour;
+          break;
+        case 'Vacation Hours (Night Sleep)':
+          totals.VHNH += hour;
+          break;
+        default:
+          break;
         }
-      });
-    });
-    return totalHours.toFixed(2);
+        })
+      })
+    setTotals(totals);
+  }
+
+  const calculateHours = (startTime, endTime) => {
+    if (!startTime || !endTime) return 0;
+    const start = new Date(`1970-01-01T${startTime}Z`);
+    const end = new Date(`1970-01-01T${endTime}Z`);
+    return Math.round((end - start) / 3600000 * 10) / 10; // in hours, rounded
   };
 
   const weeks = [];
@@ -115,172 +102,153 @@ const calculateTotalHoursByType = (tableData) => {
     }
   }, [selectedWeek]);
 
-  const calculateHours = (startTime, endTime) => {
-    if (!startTime || !endTime) return '';
-    const start = new Date(`1970-01-01T${startTime}Z`);
-    const end = new Date(`1970-01-01T${endTime}Z`);
-    const totalHour = (end - start) / 3600000; // in hours
-    const roundedTotalHour = Math.round(totalHour * 10) / 10; // Round to one decimal place
-    return roundedTotalHour;
-  };
-
-
-
   return (
     <div className="home-container">
       <h1>Hello, {firstName}</h1>
 
-      <section>
-        <h2>Timesheets</h2>
-        <div>
-          <label>Week:</label>
-          <select onChange={(e) => setSelectedWeek(e.target.value)}>
-            <option value="">Select a week</option>
-            {weeks.map((week, index) => (
-              <option key={index} value={week}>{week}</option>
-            ))}
-          </select>
-        </div>
-        </section>
+<section>
+  <h2>Timesheets</h2>
+  <div>
+    <label>Week:</label>
+    <select onChange={(e) => setSelectedWeek(e.target.value)}>
+      <option value="">Select a week</option>
+      {weeks.map((week, index) => (
+        <option key={index} value={week}>{week}</option>
+      ))}
+    </select>
+  </div>
+  </section>
 
-        {selectedWeek && (
-          <table>
-            <thead>
-              <tr>
-                <th>Day Worked</th>
-                <th>Start Time</th>
-                <th>End Time</th>
-                <th>Total Hours</th>
-                <th>Hour Type</th>
-                {/* ... other headers */}
-                <th>Member Supported</th>
-                <th>Actions</th>
+  {selectedWeek && (
+    <table>
+      <thead>
+        <tr>
+          <th>Day Worked</th>
+          <th>Start Time</th>
+          <th>End Time</th>
+          <th>Total Hours</th>
+          <th>Hour Type</th>
+          {/* ... other headers */}
+          <th>Member Supported</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {tableData.map((row, rowIndex) => (
+          <>
+            {row.entries.map((entry, entryIndex) => (
+              <tr key={`${row.date}-${entryIndex}`}>
+                <td>{entryIndex === 0 ? `${row.dayWorked} (${row.date})` : ''}</td>
+                <td>
+                  <input 
+                    type="time" 
+                    value={entry.startTime}
+                    onChange={(e) => {
+                      const updatedTableData = [...tableData];
+                      updatedTableData[rowIndex].entries[entryIndex].startTime = e.target.value;
+                      setTableData(updatedTableData);
+                    }} 
+                  />
+                </td>
+                <td>
+                  <input 
+                    type="time"
+                    value={entry.endTime}
+                    onChange={(e) => {
+                      const updatedTableData = [...tableData];
+                      updatedTableData[rowIndex].entries[entryIndex].endTime = e.target.value;
+                      setTableData(updatedTableData);
+                    }} 
+                  />
+                </td>
+                <td>{calculateHours(entry.startTime, entry.endTime)}</td>
+                <td>
+                  <select value={entry.hourType} onChange={(e) => {
+                    const updatedTableData = [...tableData];
+                    updatedTableData[rowIndex].entries[entryIndex].hourType = e.target.value;
+                    setTableData(updatedTableData);
+                  }}>
+                    {/* ... hour type options ... */}
+                    <option value="NULL">Select hour type</option>
+                  <option value="Regular Hours">Regular Hours</option>
+                  <option value="Night Awake Hours">Night Awake Hours</option>
+                  <option value="Night Sleep Hours">Night Sleep Hours</option>
+                  <option value="On Call Regular">On Call Regular</option>
+                  <option value="On Call Night Sleep">On Call Night Sleep</option>
+                  <option value="Team Lead Regular">Team Lead Regular</option>
+                  <option value="Team Lead Night Awake">Team Lead Night Awake</option>
+                  <option value="Team Lead Sleep">Team Lead Sleep</option>
+                  <option value="Training Hours">Training Hours</option>
+                  <option value="Sick Hours (Regular)">Sick Hours (Regular)</option>
+                  <option value="Sick Hours (Night Awake)">Sick Hours (Night Awake)</option>
+                  <option value="Sick Hours (Night Sleep)">Sick Hours (Night Sleep)</option>
+                  <option value="Vacation Hours (Regular)">Vacation Hours (Regular)</option>
+                  <option value="Vacation Hours (Night Awake)">Vacation Hours (Night Awake)</option>
+                  <option value="Vacation Hours (Night Sleep)">Vacation Hours (Night Sleep)</option>
+                  </select>
+                </td>
+                {/* ... other fields ... */}
+                <td>
+                  <select value={entry.memberSupported} onChange={(e) => {
+                    const updatedTableData = [...tableData];
+                    updatedTableData[rowIndex].entries[entryIndex].memberSupported = e.target.value;
+                    setTableData(updatedTableData);
+                  }}>
+
+                    {/* ... member supported options ... */}
+                  <option value="Select">Select Member</option>
+                  <option value="Yash">Yash</option>
+                  <option value="Somani">Somani</option>
+                  <option value="Isa">Isa</option>
+                  </select>
+                </td>
+                <td>
+                  <Button variant="outlined"  color="error" onClick={() => {
+                    const updatedTableData = [...tableData];
+                    updatedTableData[rowIndex].entries.splice(entryIndex, 1);
+                    setTableData(updatedTableData);
+                  }}>
+                    Remove Line
+                  </Button>
+                </td>
               </tr>
-            </thead>
-            <tbody>
-              {tableData.map((row, rowIndex) => (
-                <>
-                  {row.entries.map((entry, entryIndex) => (
-                    <tr key={`${row.date}-${entryIndex}`}>
-                      <td>{entryIndex === 0 ? `${row.dayWorked} (${row.date})` : ''}</td>
-                      <td>
-                        <input 
-                          type="time" 
-                          value={entry.startTime}
-                          onChange={(e) => {
-                            const updatedTableData = [...tableData];
-                            updatedTableData[rowIndex].entries[entryIndex].startTime = e.target.value;
-                            setTableData(updatedTableData);
-                          }} 
-                        />
-                      </td>
-                      <td>
-                        <input 
-                          type="time"
-                          value={entry.endTime}
-                          onChange={(e) => {
-                            const updatedTableData = [...tableData];
-                            updatedTableData[rowIndex].entries[entryIndex].endTime = e.target.value;
-                            setTableData(updatedTableData);
-                          }} 
-                        />
-                      </td>
-                      <td>{calculateHours(entry.startTime, entry.endTime)}</td>
-                      <td>
-                        <select value={entry.hourType} onChange={(e) => {
-                          const updatedTableData = [...tableData];
-                          updatedTableData[rowIndex].entries[entryIndex].hourType = e.target.value;
-                          setTableData(updatedTableData);
-                        }}>
-                          {/* ... hour type options ... */}
-                        <option value="Regular Hours">Regular Hours</option>
-                        <option value="Night Awake Hours">Night Awake Hours</option>
-                        <option value="Night Sleep Hours">Night Sleep Hours</option>
-                        <option value="On Call Regular">On Call Regular</option>
-                        <option value="On Call Night Sleep">On Call Night Sleep</option>
-                        <option value="Team Lead Regular">Team Lead Regular</option>
-                        <option value="Team Lead Night Awake">Team Lead Night Awake</option>
-                        <option value="Team Lead Sleep">Team Lead Sleep</option>
-                        <option value="Training Hours">Training Hours</option>
-                        <option value="Sick Hours (Regular)">Sick Hours (Regular)</option>
-                        <option value="Sick Hours (Night Awake)">Sick Hours (Night Awake)</option>
-                        <option value="Sick Hours (Night Sleep)">Sick Hours (Night Sleep)</option>
-                        <option value="Vacation Hours (Regular)">Vacation Hours (Regular)</option>
-                        <option value="Vacation Hours (Night Awake)">Vacation Hours (Night Awake)</option>
-                        <option value="Vacation Hours (Night Sleep)">Vacation Hours (Night Sleep)</option>
-                        </select>
-                      </td>
-                      {/* ... other fields ... */}
-                      <td>
-                        <select value={entry.memberSupported} onChange={(e) => {
-                          const updatedTableData = [...tableData];
-                          updatedTableData[rowIndex].entries[entryIndex].memberSupported = e.target.value;
-                          setTableData(updatedTableData);
-                        }}>
+            ))}
+            <tr>
+              <td colSpan="7">
+                <Button variant="contained" onClick={() => {
+                  const updatedTableData = [...tableData];
+                  updatedTableData[rowIndex].entries.push({
+                    startTime: '',
+                    endTime: '',
+                    hourType: '',
+                    memberSupported: 'Select'
+                  });
+                  setTableData(updatedTableData);
+                }}>
+                  Add Line
+                </Button>
 
-                          {/* ... member supported options ... */}
-                        <option value="Select">Select Member</option>
-                        <option value="Yash">Yash</option>
-                        <option value="Somani">Somani</option>
-                        <option value="Isa">Isa</option>
-                        </select>
-                      </td>
-                      <td>
-                        <button onClick={() => {
-                          const updatedTableData = [...tableData];
-                          updatedTableData[rowIndex].entries.splice(entryIndex, 1);
-                          setTableData(updatedTableData);
-                        }}>
-                          Remove Line
-                        </button>
-                      </td>
-                    </tr>
-                  ))}
-                  <tr>
-                    <td colSpan="7">
-                      <button onClick={() => {
-                        const updatedTableData = [...tableData];
-                        updatedTableData[rowIndex].entries.push({
-                          startTime: '',
-                          endTime: '',
-                          hourType: '',
-                          memberSupported: 'Select'
-                        });
-                        setTableData(updatedTableData);
-                      }}>
-                        Add Line
-                      </button>
+              </td>
+            </tr>
 
-                    </td>
-                  </tr>
+          </>
+        ))}
+      
+      <h2>Total Hours by Hour Type</h2>
+      <p>Regular Hours: {totals.RH} hours</p>
+      <p>Team Lead Regular: {totals.TL} hours</p>
+              <p>Team Lead Sleep: {totals.TLNS} hours</p>
+              <p>Night Awake Hours: {totals.NA} hours</p>
+              <p>Night Sleep Hours: {totals.NS} hours</p>
+              <p>Sick Hours: {totals.SH} hours</p>
+              <p>Sick Night Sleep Hours: {totals.SNS} hours</p>
+              <p>Vacation Hours: {totals.VHRH} hours</p>
+              <p>Vacation Night Sleep Hours: {totals.VHNH} hours</p>
+              </tbody>
+              </table>
+  )}
+    </div>
+  );
+}
 
-                </>
-              ))}
-              <h2>Total Hours by Hour Type</h2>
-)        
-      <button
-        onClick={() => {
-          const calculatedTotals = calculateTotalHoursByType(tableData);
-          
-          return (
-            <ul>
-              <li>Regular Hours: {totals.RH} hours</li>
-              <li>Team Lead Regular: {totals.TL} hours</li>
-              <li>Team Lead Sleep: {totals.TLNS} hours</li>
-              <li>Night Awake Hours: {totals.NA} hours</li>
-              <li>Night Sleep Hours: {totals.NS} hours</li>
-              <li>Sick Hours: {totals.SH} hours</li>
-              <li>Sick Night Sleep Hours: {totals.SNS} hours</li>
-              <li>Vacation Hours: {totals.VHRH} hours</li>
-              <li>Vacation Night Sleep Hours: {totals.VHNS} hours</li>
-            </ul>
-          );
-        }}
-      >
-        Calculate Hours
-      </button>
-      </tbody>
-      </table>
-  )}</div>
-     ) }
 export default Home;
